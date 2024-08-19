9798f4afa5f296cb4205f89256aebe67ee85503e
bharat balaji d <bharatbalaji95@gmail.com>
2024-08-19 18:51:07 +0530

    add

--- null.tf
+++ null.tf
@@ -1,15 +1,12 @@
 resource "null_resource" "update_kubeconfig_and_apply_autoscaler" {
   provisioner "local-exec" {
     command = <<EOT
-    aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.main.name} &&
-        CLUSTER_NAME=${aws_eks_cluster.main.name} envsubst < /Users/bharat/bharat/lab/terraform/eks/eks-terraform/k8s_resource/cluster_autoscaler.yaml | kubectl apply -f -
+    aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.main.name}
         EOT
   }
-
-  provisioner "local-exec" {
+   provisioner "local-exec" {
     when    = destroy
     command = "rm -f ~/.kube/config"
   }
-
   depends_on = [aws_eks_cluster.main]
 }
